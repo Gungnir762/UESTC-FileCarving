2f9d11d050d2d63ebb7efc0d74f74a8ea4f96d6d
Neko31662 <127868654+Neko31662@users.noreply.github.com>
2023-03-15 14:12:15 +0800

    Update FindText.h

--- FindText.h
+++ FindText.h
@@ -1,14 +1,17 @@
 #pragma once
 #include<stdio.h>
-#include"BasicFunction.h"
+#include"Sector.h"
 #include<string.h>
+#include<set>
+using namespace std;
 
-void FindText(FILE* fp,unsigned char* buffer);
+//输入文件指针，缓冲区基地址，输出路径，会调用FindHtml
+void FindText(FILE* fp, unsigned char* buffer, const char* OutputPath);
 
-//�����ļ�ָ�룬����������ַ�����·��
-void FindHtml(FILE* fp, unsigned char* buffer,const char* OutputPath);
+//输入文件指针，缓冲区基地址，输出路径，会usedSector中更新已经找到的扇区
+void FindHtml(FILE* fp, unsigned char* buffer, const char* OutputPath, set<int>& usedSector);
 
 void InitTextTable();
 
-//���ĳ�������Ƿ�Ϊ���ı������ı��᷵��1
+//检查某个扇区是否为纯文本，纯文本会返回1
 int CheckSector(unsigned char* buffer);
